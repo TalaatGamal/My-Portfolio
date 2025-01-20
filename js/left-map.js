99eacd1f9a70f89195b7c048af93d00a2bb1d97a
Talaat <talaatgamal.dev@gmail.com>
2025-01-20 02:43:16 +0200

    first commit

---
+++ js/left-map.js
@@ -0,0 +1,13 @@
+// const leftMap = document.querySelector('.left-map');
+
+// // دالة لتحديث ارتفاع العنصر
+// function setFullHeight() {
+//   const pageHeight = document.documentElement.scrollHeight; // حساب ارتفاع الصفحة بالكامل
+//   leftMap.style.height = pageHeight + 'px';
+// }
+
+// // استدعاء الدالة عند تحميل الصفحة
+// setFullHeight();
+
+// // استدعاء الدالة عند تحديث المحتوى أو عند تغيير الأبعاد
+// window.addEventListener('resize', setFullHeight);
