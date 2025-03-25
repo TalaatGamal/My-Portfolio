b0b968c7abc4f2672118a89a7eacfa320518a898
Talaat <talaatgamal.dev@gmail.com>
2025-03-25 16:55:59 +0200

    o

---
+++ main/m-into.js
@@ -0,0 +1,26 @@
+
+
+
+
+
+
+
+              
+// main-into
+              
+
+
+
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+
+
+  setTimeout(() => {
+        document.getElementById("mInto").style.opacity="0";
+  }, 1000);
+  setTimeout(() => {
+      document.getElementById("mInto").style.display = "none";
+  }, 2000);
+});
