03581fcfef72916bd73c2b861cf69e4fbf219510
Talaat <talaatgamal.dev@gmail.com>
2025-03-25 02:07:57 +0200

    i

--- js/header.js
+++ js/header.js
@@ -170,14 +170,14 @@ window.addEventListener('scroll', function () {
   let opacity = Math.min(currentScroll / 200, 0.4);
 
   header.style.backgroundColor = `rgba(0, 95, 102, ${opacity})`;
-  document.getElementById("air-sec-info").style.backgroundColor = `rgba(0, 95, 102, ${opacity})`;
+  // document.getElementById("air-sec-info").style.backgroundColor = `rgba(0, 95, 102, ${opacity})`;
   document.getElementById("air-sec-info").style.opacity="1";
 
   if (currentScroll > 0) {
     links.forEach(link => {
       link.style.color = "#ffffff";
       document.getElementById('header').querySelector(".hd-1").querySelector("a").style.color="#ffffff"
-      document.getElementById("air-sec-info").style.color="#ffffff";
+      // document.getElementById("air-sec-info").style.color="#ffffff";
       // document.getElementById("air-sec-info").style.borderBottomColor="#ffffff";
 
       document.getElementById("sidebar-menu").style.color="#ffffff";
@@ -200,7 +200,7 @@ window.addEventListener('scroll', function () {
     links.forEach(link => {
       link.style.color = "";
       document.getElementById('header').querySelector(".hd-1").querySelector("a").style.color="#4e6068"
-      document.getElementById("air-sec-info").style.color="#4e6068";
+      // document.getElementById("air-sec-info").style.color="#4e6068";
       // document.getElementById("air-sec-info").style.borderBottomColor="#4e6068";
 
       document.getElementById("sidebar-menu").style.color="#4E6068";
