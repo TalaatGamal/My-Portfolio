f20339889bb1d0df8f8e5513d9e1ebe6bb4c3f4f
Talaat <talaatgamal.dev@gmail.com>
2025-03-27 22:53:03 +0200

    op

--- js/header.js
+++ js/header.js
@@ -48,115 +48,6 @@ document.getElementById("toggle-more").addEventListener("click", function (e) {
 
 
 
-// emailjs
-
-
-
-
-// (function () {
-//   emailjs.init("3o_Ok3aG0DS4-OWJo"); 
-// })();
-
-// const formv = document.getElementById("directMessageForm-v");
-// const emailFieldv = document.getElementById("email-v-v");
-// const messageFieldv = document.getElementById("userMessage-v");
-// const feedbackv = document.getElementById("formFeedback-v");
-
-// formv.addEventListener("submit", function (e) {
-//   e.preventDefault();
-
-//   const email = emailFieldv.value.trim();
-//   const message = messageFieldv.value.trim();
-
-//   if (email === "" || message === "") {
-//     feedback.style.color = "red";
-//     feedback.textContent = "Please fill in all fields!";
-//     return;
-//   }
-
-//   const templateParams = {
-//     from_name: email,  
-//     user_message: message,
-//   };
-
-//   emailjs
-//     .send("direct-message", "template_dwzuvu4", templateParams)
-//     .then(
-//       function (response) {
-//         console.log("SUCCESS!", response.status, response.text);
-//         feedbackv.style.color = "green";
-//         feedbackv.textContent = "Message sent successfully!";
-//         formv.reset();
-//       },
-//       function (error) {
-//         console.error("FAILED...", error);
-//         feedbackv.style.color = "red";
-//         feedbackv.textContent = "Failed to send the message. Try again.";
-//       }
-//     );
-// });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// window.addEventListener('scroll', function() {
-//   const header = document.getElementById('header');
-//   let scrollY = window.scrollY || window.pageYOffset;
-  
-//   let r = 0,    // الأحمر 00
-//       g = 95,   // الأخضر 5f
-//       b = 102;  // الأزرق 66
-  
-//   let opacity = Math.min(scrollY / 200, 0.4);
-  
-//   header.style.backgroundColor = `rgba(${r}, ${g}, ${b}, ${opacity})`;
-// });
-
-// let lastScroll = 0;
-// window.addEventListener('scroll', function() {
-//   const currentScroll = window.pageYOffset;
-  
-//   if (Math.abs(currentScroll - lastScroll) < 5) return;
-  
-//   requestAnimationFrame(() => {
-//     const header = document.getElementById('header');
-//     let opacity = Math.min(currentScroll / 200, 0.4);
-//     header.style.backgroundColor = `rgba(0, 95, 102, ${opacity})`;
-
-//   //   document.getElementById('header').querySelector(".hd-2").querySelectorAll("a").forEach(function(link) {
-//   //     link.style.color = "#ffffff";
-//   // });
-
-//       lastScroll = currentScroll;
-//   });
-// });
-
-
-
-
-
-
-
-
-
 
 
 
@@ -180,6 +71,7 @@ window.addEventListener('scroll', function () {
       // document.getElementById("air-sec-info").style.color="#ffffff";
       // document.getElementById("air-sec-info").style.borderBottomColor="#ffffff";
 
+      
       document.getElementById("sidebar-menu").style.color="#ffffff";
       document.getElementById("aa-header-color2").style.cssText=`
         
