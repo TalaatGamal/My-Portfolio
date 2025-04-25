4663b1d4c1e3a228cb6db2417d6a58a8fafe21ff
Talaat <talaatgamal.dev@gmail.com>
2025-04-25 07:41:54 +0300

    o

---
+++ js/mode.js
@@ -0,0 +1,15 @@
+
+
+
+
+
+
+let mode = document.getElementById("site-mode");
+let home = document.getElementById("home");
+mode.addEventListener("click", function() {
+    
+    home.style.cssText=`
+    background-color:rgba(3, 33, 35, 0.89);
+    color:rgb(216, 216, 216);
+    `
+});
